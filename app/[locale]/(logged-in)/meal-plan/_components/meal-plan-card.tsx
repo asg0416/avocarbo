b2ba82f729aea4a0689e962d0d840de00489b720
Sujin <asg0416@naver.com>
2024-07-22 00:31:25 +0900

    meal plan card 페이지네이션 서버 컴포넌트에서 가능하도록 수정

--- app/[locale]/(logged-in)/meal-plan/_components/meal-plan-card.tsx
+++ app/[locale]/(logged-in)/meal-plan/_components/meal-plan-card.tsx
@@ -1,57 +1,53 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getMealPlansByUserId } from "@/data/meal";
-import { currentUser } from "@/lib/auth";
 import { useTranslations } from "next-intl";
-import MealPlanCardItem, { FullMealPlan } from "@/components/\bcalculator/meal-plan-card-item";
+import MealPlanCardItem, {
+  FullMealPlan,
+} from "@/components/\bcalculator/meal-plan-card-item";
 import { PaginationComponent } from "@/components/pagination";
 
 const ITEMS_PER_PAGE = 5;
 
-const MealPlanCard = () => {
-  const [currentPage, setCurrentPage] = useState(1);
+const MealPlanCard = ({
+  mealPlans,
+}: {
+  mealPlans: FullMealPlan[] | undefined | null;
+}) => {
   const t = useTranslations("meal-plan-page");
-  const [mealPlans, setMealPlans] = useState<FullMealPlan[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    const fetchMealPlans = async () => {
-      const user = await currentUser();
-      if (user?.id) {
-        const fetchedMealPlans = await getMealPlansByUserId(user.id);
-        if (fetchedMealPlans) {
-          setMealPlans(fetchedMealPlans);
-          setTotalPages(Math.ceil(fetchedMealPlans.length / ITEMS_PER_PAGE));
-        }
-      }
-    };
-    fetchMealPlans();
-  }, []);
+    if (mealPlans) {
+      setTotalPages(Math.ceil(mealPlans.length / ITEMS_PER_PAGE));
+    }
+  }, [mealPlans]);
 
-  const paginatedMealPlans = mealPlans.slice(
+  const paginatedMealPlans = mealPlans?.slice(
     (currentPage - 1) * ITEMS_PER_PAGE,
     currentPage * ITEMS_PER_PAGE
   );
 
-  if (!mealPlans.length) {
+  if (!paginatedMealPlans || !paginatedMealPlans.length) {
     return (
       <div className="p-2">
         <p>{t("empty-meal")}</p>
       </div>
     );
   }
-
-  return (
-    <div className="flex flex-col gap-y-4 p-4">
-      <MealPlanCardItem mealPlans={paginatedMealPlans} />
-      <PaginationComponent
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={(page) => setCurrentPage(page)}
-      />
-    </div>
-  );
+  if (paginatedMealPlans.length > 0) {
+    return (
+      <div className="flex flex-col gap-y-4 p-4">
+        <MealPlanCardItem mealPlans={paginatedMealPlans} />
+        <PaginationComponent
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      </div>
+    );
+  }
 };
 
 export default MealPlanCard;
--- app/[locale]/(logged-in)/meal-plan/page.tsx
+++ app/[locale]/(logged-in)/meal-plan/page.tsx
@@ -3,9 +3,14 @@ import { Title } from "@/components/\bcalculator/title";
 import { Fragment } from "react";
 import CreateMealPlanButton from "./_components/create-meal-plan-button";
 import { getTranslations } from "next-intl/server";
+import { getMealPlansByUserId } from "@/data/meal";
+import { currentUser } from "@/lib/auth";
 
 const MealPlanPage = async () => {
   const t = await getTranslations("meal-plan-page");
+  const user = await currentUser()
+  const mealPlans = await getMealPlansByUserId(user?.id || "");
+
   return (
     <Fragment>
       <div className="w-full flex items-center justify-between">
@@ -16,7 +21,7 @@ const MealPlanPage = async () => {
       </div>
 
       <div className="grow border shadow-md rounded-md w-full">
-        <MealPlanCard />
+        <MealPlanCard mealPlans={mealPlans}/>
       </div>
     </Fragment>
   );
